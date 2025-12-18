5c84635eab952883d2002c28c80f0c9de07c2fae
anbarasuanbu68543-lgtm <anbarasuanbu68543@gmail.com>
2025-12-18 19:45:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handlesend = () => {
     setstatus(true);
     axios
-      .post("https://bulkmail-app-backend-5.onrender.com", {
+      .post("https://bulkmail-app-backend-5.onrender.com/sendmail", {
         msg: msg,
         emaillist: emaillist,
       })
